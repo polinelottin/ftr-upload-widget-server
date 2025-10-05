350cfd8d75c1e3b4de82f8aad8056521356178ce
Poline Lottin <poline.lottin@goco.io>
2025-10-04 21:27:20 -0300

    chore: create ecr

--- iac/index.ts
+++ iac/index.ts
@@ -1,12 +1,23 @@
 import * as aws from "@pulumi/aws";
 
-const bucket = new aws.s3.BucketV2("teste-pos-rocketseat", {
-    bucket: 'teste-pos-rocketseat',
+const bucket = new aws.s3.BucketV2("primeiro-bucket", {
+    bucket: 'plnlttn-bucket-pos-full-stack-360',
     tags: {
         IAC: "true",
     },
 });
 
+const ecr = new aws.ecr.Repository("primeiro-ecr", {
+    name: 'plnlttn-primeiro-ecr',
+    imageTagMutability: 'IMMUTABLE',
+    tags: {
+        IAC: "true",
+    }
+});
+
 export const bucketName = bucket.id;
 export const bucketRegion = bucket.region;
 export const bucketArn = bucket.arn;
+
+export const ecrName = ecr.name;
+export const ecrRepositoryUrl = ecr.repositoryUrl;
